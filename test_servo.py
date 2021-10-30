d55c18c2d372c12c0b14d994fce59978cbe04846
JH <pwr2011@naver.com>
2021-10-30 17:47:22 +0900

    Update test_servo.py

--- test_servo.py
+++ test_servo.py
@@ -20,7 +20,7 @@ def to_left():
 
 def to_right():
     i = start_deg
-    while i<=start_deg+1.9:
+    while i<=start_deg+1.85:
         p.ChangeDutyCycle(i)
         print("angle : ",i)
         time.sleep(sleep_time)
