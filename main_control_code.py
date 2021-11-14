5f44809c7c018f70294ae96244608e9e930e577a
pwr2011 <37601696+pwr2011@users.noreply.github.com>
2021-11-14 15:50:49 +0900

    Update main_control_code.py

--- main_control_code.py
+++ main_control_code.py
@@ -14,11 +14,17 @@ import get_phone_number as phone
 import error
 import example
 from examples.lite.examples.image_classification.raspberry_pi import classify_picamera
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+import picamera
+import io
 
 #for gui
 app = QApplication(sys.argv)
 
 #Variables
+global model
 global hx
 global p
 global sleep_time
@@ -37,17 +43,61 @@ depth2 = 0
 def check_load_cell():
     return example.get_weight(hx)
 
+def init_keras():
+    global model
+    np.set_printoptions(suppress=True)
+    model = tensorflow.keras.models.load_model('keras_model.h5')
+
 # Classify the cup using TensorFlow Lite
 # return True/False
 # True : Plastic
 # False : Paper
-def tensor_flow():
-    ret,prob = classify_picamera.main()
-    print("classify result : ",ret,"probability : ",prob)
-    # if ret == 0:
-    #     return False
-    # else:
-    #     return True
+def tensor_flow(model):
+    with picamera.PiCamera(resolution=(224, 224), framerate=35) as camera:
+        try:  
+        stream = io.BytesIO()
+        for _ in camera.capture_continuous(
+            stream, format='jpeg', use_video_port=True):
+
+            
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            stream.seek(0)
+            image = Image.open(stream).convert('RGB').resize((224, 224),
+                                                            Image.ANTIALIAS)
+
+            #resize the image to a 224x224 with the same strategy as in TM2:
+            #resizing the image to be at least 224x224 and then cropping from the center
+            size = (224, 224)
+            image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+            #turn the image into a numpy array
+            image_array = np.asarray(image)
+
+            # display the resized image
+            #image.show()
+
+            # Normalize the image
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+            # Load the image into the array
+            data[0] = normalized_image_array
+
+            # run the inference
+            prediction = model.predict(data)
+            #print(prediction)
+            stream.seek(0)
+            stream.truncate()
+            #time.sleep(3)
+            max_idx = 0
+            max_prob = 0
+            for i in range(0,4):
+                if prediction[i] > max_prob:
+                    max_prob = prediction[i]
+                    max_idx= i
+            return i
+        finally:
+        camera.stop_preview()
+
 
 def point_add_gui()->str: # 입력한 전화번호를 리턴
     myWindow = phone.PhoneWindow()
@@ -65,6 +115,7 @@ def do_empty_gui():
 # main
 if __name__ == "__main__":
     #hx = example.init_hx711()
+    
     p,sleep_time,start_deg = servo.init_servo()
     Ultrasonic.ultra_init()
     #gps.gps_init()
@@ -92,7 +143,8 @@ if __name__ == "__main__":
             #    continue
 
             #
-            classify_result = tensor_flow()
+            classify_result = tensor_flow(model)
+            print("classify index : " + classify_result)
             print("here")
             time.sleep(2)
             continue
