5f47e17fd66ae8bf20a2383f53672a2d4be8a8b9
pwr2011 <37601696+pwr2011@users.noreply.github.com>
2021-11-14 16:12:25 +0900

    Update main_control_code.py

--- main_control_code.py
+++ main_control_code.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
 
             #
             classify_result = tensor_flow(model)
-            print("classify index : " + classify_result)
+            print("classify index : " + str(classify_result))
             print("here")
             time.sleep(2)
             continue
